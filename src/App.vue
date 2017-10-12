fcd4b6b5e6cab8ffd8745cfdc5372c313250c7ef
zwq <zwq@126.com>
2017-10-12 22:16:41 +0800

    根组件APP的实现

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <mt-header fixed title="vue练习项目"></mt-header>
     <router-view/>
+    <!-- mui 九宫格样式-->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email"></span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
   </div>
 </template>
 
@@ -11,13 +30,14 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="less">
+  .mint-header{
+    height: 2rem;
+    font-size: 1rem
+  }
+  .mui-bar-tab{
+    .mui-tab-item{
+      line-height: 70%;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import mint from 'mint-ui';
+Vue.use(mint);
+import 'mint-ui/lib/style.min.css';
+import '../static/mui/css/mui.min.css';
+// 初始化的样式
+import '../static/base.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +18,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
